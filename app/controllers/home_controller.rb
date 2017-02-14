447b4db432e9c4c661eb3b0987a61e2d5d7f7f0d
lanhhoang <lanh.hoangcong@gmail.com>
2017-02-14 15:23:32 +0700

    Add 4 sections to menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def menu
+    @sections = Section.all
+    if params[:section_id].present?
+      @current_section = Section.find(params[:section_id])
+    end
+  end
+
+  def contact_us
+  end
 end
