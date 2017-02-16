aedaaea6f5c7abdf5111edac1e90969c9d3c8262
lanhhoang <lanh.hoangcong@gmail.com>
2017-02-16 16:14:46 +0700

    Add order button for menu item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :food_items
+  resources :food_items do
+    resources :orders
+  end
   get 'menu' => 'home#menu'
   get 'contact_us' => 'home#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
